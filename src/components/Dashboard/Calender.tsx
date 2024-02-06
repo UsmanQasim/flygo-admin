fd1e6208349a09dbb7cea7e6e09697c6eda7bc46
UsmanQasim <usman.qasim@whetstonez.com>
2024-02-06 17:55:46 +0500

    init

---
+++ src/components/Dashboard/Calender.tsx
@@ -0,0 +1,26 @@
+"use client";
+// import dynamic from "next/dynamic";
+import React, { useState } from "react";
+import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from "react-datepicker";
+
+const Calender = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  // const DatePicker = dynamic(() => import("react-datepicker"), { ssr: false });
+
+  return (
+    <div className="col-lg-6 col-xxl-4">
+      <div className="datepicker-dashboard card">
+        <div className="datepicker-here">
+          <DatePicker
+            selected={startDate}
+            onChange={(date: Date) => setStartDate(date)}
+            inline
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Calender;
